3a64f091da12084bbbf932ba81c1d0b3a6de1c75
Rick Wong <wong@mollie.nl>
2013-12-09 10:42:00 +0100

    Minor update: copyright notices.

--- src/Mollie/API/Object/List.php
+++ src/Mollie/API/Object/List.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2014, Mollie B.V.
+ * Copyright (c) 2013, Mollie B.V.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
