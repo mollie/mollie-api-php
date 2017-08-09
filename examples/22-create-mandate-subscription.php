4258488c10847208b78f0a77f6673a4db2814a7b
Willem Stuursma <willem@mollie.nl>
2017-08-09 13:16:11 +0200

    Make sure examples can be run in all supported PHP versions too

--- examples/22-create-mandate-subscription.php
+++ examples/22-create-mandate-subscription.php
@@ -19,24 +19,24 @@ try
     echo "<p>Customer created with id ". $customer->id."</p>";
 
     // create mandate
-    $mandate = $mollie->customers_mandates->withParentId($customer->id)->create([
+    $mandate = $mollie->customers_mandates->withParentId($customer->id)->create(array(
         "method" => 'directdebit',
         "consumerAccount" => 'NL12ABNA654065985',
         "consumerName" => 'B. A. Example',
-    ]);
+    ));
     echo "<p>Mandate created with id ". $mandate->id."</p>";
 
     // set recurring
-    $subscription = $mollie->customers_subscriptions->withParentId($customer->id)->create([
+    $subscription = $mollie->customers_subscriptions->withParentId($customer->id)->create(array(
         "amount"      => 10.00,
         "times"       => 12, // recurring membership for 1 year
         "interval"    => "1 months", // every month
         "description" => "Subscription ".$membership_id,
         "webhookUrl"  => "https://example.com/webhook.php",
-        "metadata" => [
+        "metadata" => array(
             "order_id" => $membership_id,
-        ],
-    ]);
+        ),
+    ));
 
     echo "<p>Subscription created with id ". $subscription->id."</p>";
 }
@@ -44,4 +44,3 @@ catch (Mollie_API_Exception $e)
 {
     error_log( "API call failed: " . htmlspecialchars($e->getMessage()));
 }
-?>
