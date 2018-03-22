dfe53176980c6f0a907468872f7f557a90d6409c
Daan van Marsbergen <daan@mollie.com>
2018-03-22 16:36:11 +0100

    Use the composer autoloader

--- examples/initialize_with_oauth.php
+++ examples/initialize_with_oauth.php
@@ -9,7 +9,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once dirname(__FILE__) . "/../src/Mollie/API/Autoloader.php";
+require_once dirname(__FILE__) . "/../vendor/autoload.php";
 
 /*
  * Initialize the Mollie API library with OAuth.
