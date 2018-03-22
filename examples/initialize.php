f0c5341f30cd1f76041c96bbb40659b3a2195556
Daan van Marsbergen <daan@mollie.com>
2018-03-22 16:34:59 +0100

    Display errors by default in examples

--- examples/initialize.php
+++ examples/initialize.php
@@ -2,6 +2,13 @@
 
 use Mollie\Api\MollieApiClient;
 
+/*
+ * Make sure to disable the display of errors in production code!
+ */
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once dirname(__FILE__) . "/../vendor/autoload.php";
 
 /*
