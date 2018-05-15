dc3fb4ba23ef9592f1262ab82147c6ef61b566aa
Martijn Smit <smit@mollie.com>
2018-05-15 15:41:22 +0200

    Move back the customer delete example

---
+++ examples/23-delete-customer.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Example 23 - Delete a customer from mollie api.
+*/
+
+try {
+    /*
+ * Initialize the Mollie API library with your API key or OAuth access token.
+ */
+    require "initialize.php";
+
+    $mollie->customers->delete("customer_id");
+    echo "Customer deleted!";
+
+} catch (\Mollie\Api\Exceptions\ApiException $e) {
+    error_log("API call failed: " . htmlspecialchars($e->getMessage()));
+}
