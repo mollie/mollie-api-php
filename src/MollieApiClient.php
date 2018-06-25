4ffda32886ebec47fd1cc627ed9409b9664a969c
Willem Stuursma <willem@mollie.nl>
2018-06-25 14:06:49 +0200

    Update client version

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -26,7 +26,7 @@ class MollieApiClient
     /**
      * Version of our client.
      */
-    const CLIENT_VERSION = "2.0.0";
+    const CLIENT_VERSION = "2.0.5";
 
     /**
      * Endpoint of the remote API.
