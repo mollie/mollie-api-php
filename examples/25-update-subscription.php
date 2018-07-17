82619a3bf382f7eee7433de1fa4b5fe27372e618
Igor Budasov <igor.budasov@mollie.com>
2018-07-17 15:58:36 +0200

    Code style

--- examples/25-update-subscription.php
+++ examples/25-update-subscription.php
@@ -13,7 +13,7 @@ try {
     require "./initialize.php";
 
     /*
-     * Retrieve an existing subscription by its ID and the customer
+     * Retrieve an existing subscription
      */
     $customer = $mollie->customers->get("cst_cUe8HjeBuz");
     $subscription = $customer->getSubscription("sub_DRjwaT5qHx");
@@ -23,13 +23,13 @@ try {
      * @See https://docs.mollie.com/reference/v2/subscriptions-api/update-subscription
      */
     $subscription->times = 10;
-    $subscription->startDate = (new DateTime('tomorrow'))->format('Y-m-d');
+    $subscription->startDate = '2018-12-02'; // Year-month-day
     $subscription->amount = (object)['amount' => '12.12', 'currency' => 'EUR'];
     $subscription->webhookUrl = 'https://some-webhook-url.com/with/path';
     $subscription->description = 'Monthly subscription';
     $subscription->update();
 
-    echo "<p>Subscription updated: " . htmlspecialchars($subscription->id) . "</p>";
+    echo "<p>Subscription updated: " . $subscription->id . "</p>";
 } catch (ApiException $e) {
     echo "API call failed: " . htmlspecialchars($e->getMessage());
 }
