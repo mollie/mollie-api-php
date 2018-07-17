c466863cd766968fc4220701b5575246fa515964
Igor Budasov <igor.budasov@mollie.com>
2018-07-17 15:12:43 +0200

    Customer update now uses PATCH method

--- tests/Mollie/API/Endpoints/CustomerEndpointTest.php
+++ tests/Mollie/API/Endpoints/CustomerEndpointTest.php
@@ -161,4 +161,71 @@ class CustomerEndpointTest extends BaseEndpointTest
 
     }
 
+    public function testUpdateWorks()
+    {
+        $expectedName = 'Kaas Broodje';
+        $expectedEmail = 'kaas.broodje@gmail.com';
+
+        $this->mockApiCall(
+            new Request('PATCH', '/v2/customers/cst_FhQJRw4s2n'),
+            new Response(
+                200,
+                [],
+                '{
+                  "resource": "customer",
+                  "id": "cst_FhQJRw4s2n",
+                  "mode": "test",
+                  "name": "'.$expectedName.'",
+                  "email": "'.$expectedEmail.'",
+                  "locale": null,
+                  "metadata": null,
+                  "recentlyUsedMethods": [],
+                  "createdAt": "2018-04-19T08:49:01+00:00",
+                  "_links": {
+                    "documentation": {
+                      "href": "https://docs.mollie.com/reference/v2/customers-api/get-customer",
+                      "type": "text/html"
+                    }
+                  }
+                }'
+            )
+        );
+
+        $customer = $this->getCustomer();
+        $customer->name = $expectedName;
+        $customer->email = $expectedEmail;
+
+        $customer->update();
+    }
+
+    /**
+     * @return Customer
+     */
+    private function getCustomer()
+    {
+        $customerJson = '{
+                  "resource": "customer",
+                  "id": "cst_FhQJRw4s2n",
+                  "mode": "test",
+                  "name": "John Doe",
+                  "email": "johndoe@example.org",
+                  "locale": null,
+                  "metadata": null,
+                  "recentlyUsedMethods": [],
+                  "createdAt": "2018-04-19T08:49:01+00:00",
+                  "_links": {
+                    "self": {
+                        "href": "http://api.mollie.test/v2/customers/cst_FhQJRw4s2n",
+                        "type": "application/hal+json"
+                    },
+                    "documentation": {
+                      "href": "https://docs.mollie.com/reference/v2/customers-api/get-customer",
+                      "type": "text/html"
+                    }
+                  }
+                }';
+
+        return $this->copy(json_decode($customerJson), new Customer($this->apiClient));
+    }
+
 }
