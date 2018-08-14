7664d5359392e99210b02ee5f8b806b059865e0e
Sander van Hooft <info@sandervanhooft.nl>
2018-08-14 16:23:58 +0200

    Explicit definition of Shipment attributes

--- src/Resources/Shipment.php
+++ src/Resources/Shipment.php
@@ -10,11 +10,40 @@ class Shipment extends BaseResource
     public $resource;
 
     /**
-     * Id of the shipment.
+     * The shipment’s unique identifier,
      *
+     * @example shp_3wmsgCJN4U
      * @var string
      */
     public $id;
+
+    /**
+     * Id of the order.
+     *
+     * @example ord_8wmqcHMN4U
+     * @var string
+     */
+    public $orderId;
+
+    /**
+     * UTC datetime the shipment was created in ISO-8601 format.
+     *
+     * @example "2013-12-25T10:30:54+00:00"
+     * @var string|null
+     */
+    public $createdAt;
+
+    /**
+     * The order object lines contain the actual things the customer bought.
+     * @var array|object[]
+     */
+    public $lines;
+
+    /**
+     * An object with several URL objects relevant to the customer. Every URL object will contain an href and a type field.
+     * @var object[]
+     */
+    public $_links;
 
     /**
      * Get the line value objects
