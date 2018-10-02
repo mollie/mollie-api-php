1b150177adc0a004a9ed305782cb6055995591db
Daan van Marsbergen <daan@mollie.com>
2018-10-02 15:25:21 +0200

    Add new 'authorized' payment status for Klarna payments

--- src/Types/PaymentStatus.php
+++ src/Types/PaymentStatus.php
@@ -15,6 +15,16 @@ class PaymentStatus
     const STATUS_PENDING = "pending";
 
     /**
+     * The payment is authorized, but captures still need to be created in order to receive the money.
+     *
+     * This is currently only possible for Klarna Pay later and Klarna Slice it. Payments with these payment methods can
+     * only be created with the Orders API. You should create a Shipment to trigger the capture to receive the money.
+     *
+     * @see https://docs.mollie.com/reference/v2/shipments-api/create-shipment
+     */
+    const STATUS_AUTHORIZED = "authorized";
+
+    /**
      * The customer has canceled the payment.
      */
     const STATUS_CANCELED = "canceled";
