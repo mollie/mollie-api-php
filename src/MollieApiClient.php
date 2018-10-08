efc7dc641fb6a22c0a9617475e4388f99e8d78d9
Martijn Smit <smit@mollie.com>
2018-10-08 10:57:59 +0200

    Bump the version

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -35,7 +35,7 @@ class MollieApiClient
     /**
      * Version of our client.
      */
-    const CLIENT_VERSION = "2.1.1";
+    const CLIENT_VERSION = "2.1.2";
 
     /**
      * Endpoint of the remote API.
