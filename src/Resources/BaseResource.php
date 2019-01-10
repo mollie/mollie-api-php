37f30dde2599f558dca3b1b2fb9ff5565c67d6a4
Sander van Hooft <info@sandervanhooft.nl>
2019-01-10 16:51:23 +0100

    Pull up method createResourceCollection

--- src/Resources/BaseResource.php
+++ src/Resources/BaseResource.php
@@ -18,4 +18,27 @@ abstract class BaseResource
     {
         $this->client = $client;
     }
+
+    /**
+     * Create a resource collection from an array.
+     *
+     * @param array $input
+     * @param string $resourceClass The full class namespace
+     * @param null|object[] $_links
+     * @param null $resourceCollectionClass If empty, appends 'Collection' to the `$resourceClass` to resolve the Collection class.
+     * @return mixed
+     */
+    protected function createResourceCollection($input, $resourceClass, $_links = null, $resourceCollectionClass = null)
+    {
+        if (null === $resourceCollectionClass) {
+            $resourceCollectionClass = $resourceClass.'Collection';
+        }
+
+        $data = new $resourceCollectionClass(count($input), $_links);
+        foreach ($input as $item) {
+            $data[] = ResourceFactory::createFromApiResult($item, new $resourceClass($this->client));
+        }
+
+        return $data;
+    }
 }
