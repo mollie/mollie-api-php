9b952f325d180aff5976de3edae319a7d98c3bd7
Sander van Hooft <info@sandervanhooft.nl>
2019-09-25 17:25:54 +0200

    List all subscriptions from client root

--- src/Endpoints/SubscriptionEndpoint.php
+++ src/Endpoints/SubscriptionEndpoint.php
@@ -2,9 +2,8 @@
 
 namespace Mollie\Api\Endpoints;
 
-use Mollie\Api\Resources\BaseCollection;
-use Mollie\Api\Resources\BaseResource;
 use Mollie\Api\Resources\Customer;
+use Mollie\Api\Resources\ResourceFactory;
 use Mollie\Api\Resources\Subscription;
 use Mollie\Api\Resources\SubscriptionCollection;
 
@@ -93,5 +92,33 @@ class SubscriptionEndpoint extends CollectionEndpointAbstract
         $this->parentId = $customer->id;
 
         return parent::rest_delete($subscriptionId, $data);
+    }
+
+    /**
+     * Retrieves a collection of Subscriptions from Mollie.
+     *
+     * @param string $from The first payment ID you want to include in your list.
+     * @param int $limit
+     * @param array $parameters
+     *
+     * @return SubscriptionCollection
+     * @throws \Mollie\Api\Exceptions\ApiException
+     */
+    public function page($from = null, $limit = null, array $parameters = [])
+    {
+        $filters = array_merge(["from" => $from, "limit" => $limit], $parameters);
+
+        $apiPath = 'subscriptions' . $this->buildQueryString($filters);
+
+        $result = $this->client->performHttpCall(self::REST_LIST, $apiPath);
+
+        /** @var SubscriptionCollection $collection */
+        $collection = $this->getResourceCollectionObject($result->count, $result->_links);
+
+        foreach ($result->_embedded->{$collection->getCollectionResourceName()} as $dataResult) {
+            $collection[] = ResourceFactory::createFromApiResult($dataResult, $this->getResourceObject());
+        }
+
+        return $collection;
     }
 }
