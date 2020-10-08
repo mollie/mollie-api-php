bc7f37001c7533ccf73a536e810720ce28c3c2f8
Sander van Hooft <info@sandervanhooft.nl>
2020-10-08 10:00:19 +0200

    formatting

--- tests/Guzzle/RetryMiddlewareFactoryTest.php
+++ tests/Guzzle/RetryMiddlewareFactoryTest.php
@@ -13,13 +13,39 @@ use PHPUnit\Framework\TestCase;
 
 class RetryMiddlewareFactoryTest extends TestCase
 {
-    public function testRetriesConnectException() {
+    public function testRetriesConnectException()
+    {
         $middlewareFactory = new RetryMiddlewareFactory;
 
         $mock = new MockHandler(
             array(
-                new ConnectException("Error 1", new Request( 'GET', 'test' )),
-                new Response(200, [ 'X-Foo' => 'Bar' ]),
+                new ConnectException("Error 1", new Request('GET', 'test')),
+                new Response(200, ['X-Foo' => 'Bar']),
+            )
+        );
+
+        $handler = HandlerStack::create($mock);
+        $handler->push($middlewareFactory->retry(false));
+        $client = new Client(['handler' => $handler]);
+
+        $finalResponse = $client->request('GET', '/');
+
+        $this->assertEquals(200, $finalResponse->getStatusCode());
+    }
+
+    public function testRetryLimit()
+    {
+        $middlewareFactory = new RetryMiddlewareFactory;
+        $finalException = new ConnectException("Error 6", new Request('GET', 'test'));
+
+        $mock = new MockHandler(
+            array(
+                new ConnectException("Error 1", new Request('GET', 'test')),
+                new ConnectException("Error 2", new Request('GET', 'test')),
+                new ConnectException("Error 3", new Request('GET', 'test')),
+                new ConnectException("Error 4", new Request('GET', 'test')),
+                new ConnectException("Error 5", new Request('GET', 'test')),
+                $finalException,
             )
         );
 
@@ -27,54 +53,31 @@ class RetryMiddlewareFactoryTest extends TestCase
         $handler->push($middlewareFactory->retry(false));
         $client = new Client([ 'handler' => $handler ]);
 
-        $finalResponse = $client->request('GET', '/');
-
-        $this->assertEquals(200, $finalResponse->getStatusCode());
-    }
-
-    public function testRetryLimit() {
-        $middlewareFactory = new RetryMiddlewareFactory;
-        $finalException = new ConnectException( "Error 6", new Request( 'GET', 'test' ) );
-
-        $mock = new MockHandler(
-            array(
-                new ConnectException( "Error 1", new Request( 'GET', 'test' ) ),
-                new ConnectException( "Error 2", new Request( 'GET', 'test' ) ),
-                new ConnectException( "Error 3", new Request( 'GET', 'test' ) ),
-                new ConnectException( "Error 4", new Request( 'GET', 'test' ) ),
-                new ConnectException( "Error 5", new Request( 'GET', 'test' ) ),
-                $finalException,
-            )
-        );
-
-        $handler = HandlerStack::create( $mock );
-        $handler->push( $middlewareFactory->retry( false ) );
-        $client = new Client( [ 'handler' => $handler ] );
-
         $this->expectExceptionObject($finalException);
 
         $client->request( 'GET', '/' )->getStatusCode();
     }
 
-    public function testRetryDelay() {
+    public function testRetryDelay()
+    {
         $middlewareFactory = new RetryMiddlewareFactory;
 
         $mock = new MockHandler(
             array(
-                new ConnectException( "+1 second delay", new Request( 'GET', 'test' ) ),
-                new ConnectException( "+2 second delay", new Request( 'GET', 'test' ) ),
-                new Response( 200 ),
+                new ConnectException("+1 second delay", new Request('GET', 'test')),
+                new ConnectException("+2 second delay", new Request('GET', 'test')),
+                new Response(200),
             )
         );
 
-        $handler = HandlerStack::create( $mock );
-        $handler->push( $middlewareFactory->retry( true ) );
-        $client = new Client( [ 'handler' => $handler ] );
+        $handler = HandlerStack::create($mock);
+        $handler->push($middlewareFactory->retry(true));
+        $client = new Client([ 'handler' => $handler ]);
 
         $startTime = time();
-        $client->request( 'GET', '/' )->getStatusCode();
+        $client->request('GET', '/')->getStatusCode();
         $endTime = time();
 
-        $this->assertGreaterThan( $startTime + 2, $endTime );
+        $this->assertGreaterThan($startTime + 2, $endTime);
     }
 }
