c7a587bbecee75eab093176a6620097853b75331
Sander van Hooft <info@sandervanhooft.nl>
2020-10-08 11:09:50 +0200

    Remove unused import

--- tests/Mollie/Guzzle/RetryMiddlewareFactoryTest.php
+++ tests/Mollie/Guzzle/RetryMiddlewareFactoryTest.php
@@ -8,7 +8,6 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
-use Mollie\Api\Exceptions\ApiException;
 use Mollie\Api\Guzzle\RetryMiddlewareFactory;
 use PHPUnit\Framework\TestCase;
 
