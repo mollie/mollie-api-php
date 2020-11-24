bfb5574113012be6e13ed56215f272c577abfc0b
Sander van Hooft <info@sandervanhooft.nl>
2020-11-24 14:35:21 +0100

    Rename to PENDING_BOARDING

--- src/Types/PaymentMethodStatus.php
+++ src/Types/PaymentMethodStatus.php
@@ -17,7 +17,7 @@ class PaymentMethodStatus
      *
      * @link https://docs.mollie.com/reference/v2/methods-api/get-method#parameters
      */
-    const PENDING_ONBOARDING = "pending-boarding";
+    const PENDING_BOARDING = "pending-boarding";
 
     /**
      * Mollie needs to review your request for this payment method before it can be activated.
