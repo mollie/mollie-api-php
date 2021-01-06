8415c0da3feec3794f5fce07ce3504d4516ff0a1
Mateus Revoredo <mateus.revoredo@mollie.com>
2021-01-06 15:42:31 +0100

    Update docblock to reflect new amountChargedBack value

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -45,7 +45,7 @@ class MollieApiClient
     /**
      * Version of our client.
      */
-    const CLIENT_VERSION = "2.27.1";
+    const CLIENT_VERSION = "2.27.2";
 
     /**
      * Endpoint of the remote API.
