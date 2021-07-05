ae1d7a918a4a57334170fd9092ccb736a35ad89e
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2021-07-05 05:42:15 +0200

    Create payment link example fix

--- examples/payment-links/create-payment-link.php
+++ examples/payment-links/create-payment-link.php
@@ -19,27 +19,24 @@ try {
     $path = dirname(isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $_SERVER['PHP_SELF']);
 
     /*
-     * Payment Link parameters:
+     * Required Payment Link parameters:
      *   amount        Amount in EUROs. This example creates a € 10,- payment.
      *   description   Description of the payment.
-     *   redirectUrl   Redirect location. The customer will be redirected there after the payment.
-     *   webhookUrl    Webhook location, used to report when the payment changes state.
-     *   metadata      Custom metadata that is stored with the payment.
      */
-    $paymentLink = $mollie->paymentLink->create([
+    $paymentLink = $mollie->paymentLinks->create([
         "amount" => [
             "currency" => "EUR",
             "value" => "10.00", // You must send the correct number of decimals, thus we enforce the use of strings
         ],
         "description" => "Bicycle tires",
-        "webhookUrl" => "{$protocol}://{$hostname}{$path}/webhook.php",
+        "webhookUrl" => "{$protocol}://{$hostname}{$path}/webhook.php", // optional
     ]);
 
     /*
      * Send the customer off to complete the payment.
      * This request should always be a GET, thus we enforce 303 http response code
      */
-    header("Location: " . $paymentLink->getPaymentLinkUrl(), true, 303);
+    header("Location: " . $paymentLink->getCheckoutUrl(), true, 303);
 } catch (\Mollie\Api\Exceptions\ApiException $e) {
     echo "API call failed: " . htmlspecialchars($e->getMessage());
 }
