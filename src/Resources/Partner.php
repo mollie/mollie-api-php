fde09b0bc5317ec1988a2cf76e03a958b6ef34b7
sandervanhooft <sandervanhooft@users.noreply.github.com>
2022-01-17 09:35:38 +0000

    Fix styling

--- src/Resources/Partner.php
+++ src/Resources/Partner.php
@@ -12,14 +12,14 @@ class Partner extends BaseResource
     /**
      * Indicates the type of partner. Will be null if the currently authenticated organization is
      * not enrolled as a partner. Possible values: "oauth", "signuplink", "useragent".
-     * 
+     *
      * @var string
      */
     public $partnerType;
 
     /**
      * Will be true if partner is receiving commissions. Will be null otherwise.
-     * 
+     *
      * @var bool|null
      */
     public $isCommissionPartner;
@@ -35,7 +35,7 @@ class Partner extends BaseResource
     /**
      * The date and time the contract was signed, in ISO 8601 format. Will be null if the contract has
      * not yet been signed, or if "partnerType" is null.
-     * 
+     *
      * @var string|null
      */
     public $partnerContractSignedAt;
