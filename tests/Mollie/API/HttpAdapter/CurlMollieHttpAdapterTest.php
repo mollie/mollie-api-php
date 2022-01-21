583405927609b63faa327e1f177cbeadd3df7beb
Sander van Hooft <info@sandervanhooft.nl>
2022-01-21 14:12:47 +0100

    Add debugging again to curl adapter

---
+++ tests/Mollie/API/HttpAdapter/CurlMollieHttpAdapterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Mollie\API\HttpAdapter;
+
+use Mollie\Api\Exceptions\ApiException;
+use Mollie\Api\HttpAdapter\CurlMollieHttpAdapter;
+use PHPUnit\Framework\TestCase;
+
+class CurlMollieHttpAdapterTest extends TestCase
+{
+    /** @test */
+    public function testDebuggingIsNotSupported()
+    {
+        $adapter = new CurlMollieHttpAdapter;
+        $this->assertFalse($adapter->supportsDebugging());
+    }
+}
