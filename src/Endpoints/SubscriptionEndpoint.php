3973406f175dbf347651392fa007daf7e0dee82b
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2022-03-04 14:39:35 +0200

    Update SubscriptionEndpoint.php

--- src/Endpoints/SubscriptionEndpoint.php
+++ src/Endpoints/SubscriptionEndpoint.php
@@ -4,6 +4,7 @@ namespace Mollie\Api\Endpoints;
 
 use Mollie\Api\Exceptions\ApiException;
 use Mollie\Api\Resources\Customer;
+use Mollie\Api\Resources\Order;
 use Mollie\Api\Resources\ResourceFactory;
 use Mollie\Api\Resources\Subscription;
 use Mollie\Api\Resources\SubscriptionCollection;
@@ -82,7 +83,7 @@ class SubscriptionEndpoint extends CollectionEndpointAbstract
      *
      * @param array $data
      *
-     * @return \Mollie\Api\Resources\BaseResource|null
+     * @return Order
      * @throws ApiException
      */
     public function update($customerId, $subscriptionId, array $data = [])
