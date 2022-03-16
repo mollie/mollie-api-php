14fbdf56870577bf6b073e1084fef5a097a64321
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2022-03-16 14:22:22 +0200

    wip

--- src/Resources/Refund.php
+++ src/Resources/Refund.php
@@ -5,10 +5,6 @@ namespace Mollie\Api\Resources;
 use Mollie\Api\MollieApiClient;
 use Mollie\Api\Types\RefundStatus;
 
-/**
-* @property int $count
-*/
-
 class Refund extends BaseResource
 {
     /**
