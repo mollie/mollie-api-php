a85062958896f7f6b97e0352c8bacca4e0b4ed84
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2022-06-27 10:56:33 +0200

    Simplify version number

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -44,7 +44,7 @@ class MollieApiClient
     /**
      * Version of our client.
      */
-    const CLIENT_VERSION = "0.2.balances-beta";
+    const CLIENT_VERSION = "2.balances-beta";
 
     /**
      * Endpoint of the remote API.
