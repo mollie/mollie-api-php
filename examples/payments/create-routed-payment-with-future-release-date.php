6141708bd5b33399e3f68fb9210471975a024957
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2022-07-23 14:52:46 +0300

    fixes

--- examples/payments/create-routed-payment-with-future-release-date.php
+++ examples/payments/create-routed-payment-with-future-release-date.php
@@ -4,8 +4,8 @@
  */
 
 try {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 
     require_once __DIR__ . "/../vendor/autoload.php";
