78541fe061dd03716022bf6ecda984a4ed6ef27b
Sander van Hooft <sandervanhooft@users.noreply.github.com>
2022-11-03 15:56:41 +0100

    Update version number

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -41,7 +41,7 @@ class MollieApiClient
     /**
      * Version of our client.
      */
-    public const CLIENT_VERSION = "2.46.0";
+    public const CLIENT_VERSION = "2.47.0";
 
     /**
      * Endpoint of the remote API.
