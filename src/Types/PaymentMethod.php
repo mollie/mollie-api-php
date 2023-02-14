f2f694ce26ef37362bdffe7b230b4d89f1c74e2d
Sander van Hooft <info@sandervanhooft.com>
2023-02-14 09:46:01 +0100

    Added billie payment method

--- src/Types/PaymentMethod.php
+++ src/Types/PaymentMethod.php
@@ -25,6 +25,11 @@ class PaymentMethod
     public const BELFIUS = "belfius";
 
     /**
+     * @link https://www.mollie.com/en/payments/billie
+     */
+    public const BILLIE = "billie";
+
+    /**
      * @deprecated 2019-05-01
      */
     public const BITCOIN = "bitcoin";
