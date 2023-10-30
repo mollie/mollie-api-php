b6c2b11afaa1a086a83bdbefe8d6346142b35289
Naoray <Naoray@users.noreply.github.com>
2023-10-30 14:26:37 +0000

    Fix styling

--- src/Resources/LazyCollection.php
+++ src/Resources/LazyCollection.php
@@ -75,7 +75,7 @@ class LazyCollection implements IteratorAggregate
         $iterator = $this->getIterator();
 
         if (is_null($callback)) {
-            if (!$iterator->valid()) {
+            if (! $iterator->valid()) {
                 return null;
             }
 
@@ -118,7 +118,7 @@ class LazyCollection implements IteratorAggregate
             $iterator = $this->getIterator();
 
             while ($limit--) {
-                if (!$iterator->valid()) {
+                if (! $iterator->valid()) {
                     break;
                 }
 
@@ -142,7 +142,7 @@ class LazyCollection implements IteratorAggregate
         $iterator = $this->getIterator();
 
         foreach ($iterator as $key => $value) {
-            if (!$callback($value, $key)) {
+            if (! $callback($value, $key)) {
                 return false;
             }
         }
