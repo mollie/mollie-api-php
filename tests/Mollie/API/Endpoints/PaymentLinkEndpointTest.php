2efbacf4459add2f15d205b304cfb58b27dc52d2
Sander van Hooft <info@sandervanhooft.com>
2024-06-21 13:57:03 +0200

    wip

---
+++ tests/Mollie/API/Endpoints/PaymentLinkEndpointTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Mollie\API\Endpoints;
+
+class PaymentLinkEndpointTest extends BaseEndpointTest
+{
+    /** @test */
+    public function testListPaymentLinks()
+    {
+        $this->markTestIncomplete('To be implemented');
+    }
+
+    /** @test */
+    public function testGetPaymentLink()
+    {
+        $this->markTestIncomplete('To be implemented');
+    }
+
+    /** @test */
+    public function testCreatePaymentLink()
+    {
+        $this->markTestIncomplete('To be implemented');
+    }
+
+    /** @test */
+    public function testUpdatePaymentLink()
+    {
+        $this->markTestIncomplete('To be implemented');
+    }
+
+    /** @test */
+    public function testDeletePaymentLink()
+    {
+        $this->markTestIncomplete('To be implemented');
+    }
+}
