efc93ae26042fb36597d8cd5420ebd45825dfc59
Krishan Koenig <krishan.koenig@googlemail.com>
2025-03-13 14:30:33 +0100

    chore: increment client version

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -116,7 +116,7 @@ class MollieApiClient implements Connector
     /**
      * Version of our client.
      */
-    public const CLIENT_VERSION = '3.0.0-beta.2';
+    public const CLIENT_VERSION = '3.0.0-beta.3';
 
     /**
      * Endpoint of the remote API.
