04fcb43349bbc39b8c6f5f373f15767d37baac6f
Krishan Koenig <krishan.koenig@googlemail.com>
2025-03-19 21:39:56 +0100

    chore: update client version

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -116,7 +116,7 @@ class MollieApiClient implements Connector
     /**
      * Version of our client.
      */
-    public const CLIENT_VERSION = '3.0.0-beta.5';
+    public const CLIENT_VERSION = '3.0.0-beta.6';
 
     /**
      * Endpoint of the remote API.
