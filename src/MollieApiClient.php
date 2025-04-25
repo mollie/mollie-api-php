c003460cb8c425e69ec8ca90cb3ace1c813dab3e
Krishan König <Naoray@users.noreply.github.com>
2025-04-25 11:52:19 +0200

    Update MollieApiClient.php

--- src/MollieApiClient.php
+++ src/MollieApiClient.php
@@ -116,7 +116,7 @@ class MollieApiClient implements Connector
     /**
      * Version of our client.
      */
-    public const CLIENT_VERSION = '3.0.1';
+    public const CLIENT_VERSION = '3.0.4';
 
     /**
      * Endpoint of the remote API.
