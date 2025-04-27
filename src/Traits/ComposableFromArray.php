7f89e3031ee8851593985f8e65a32988d2804ded
Krishan Koenig <krishan.koenig@googlemail.com>
2025-04-27 15:06:01 +0200

    fix: use reflection to order props in case they have not been ordered as intended

--- src/Traits/ComposableFromArray.php
+++ src/Traits/ComposableFromArray.php
@@ -2,17 +2,34 @@
 
 namespace Mollie\Api\Traits;
 
+use Mollie\Api\Contracts\Arrayable;
 use Mollie\Api\Http\Data\DataCollection;
+use ReflectionClass;
+use ReflectionParameter;
 
 trait ComposableFromArray
 {
     public static function fromArray($data = []): self
     {
-        $data = DataCollection::wrap($data)
-            ->values()
-            ->toArray();
+        $data = $data instanceof Arrayable ? $data->toArray() : $data;
 
-        /** @phpstan-ignore-next-line */
-        return new static(...$data);
+        $reflection = new ReflectionClass(static::class);
+        $parameters = $reflection->getConstructor()?->getParameters();
+
+        return DataCollection::wrap($parameters)
+            ->map(function (ReflectionParameter $parameter) use ($data) {
+                $name = $parameter->getName();
+
+                if (array_key_exists($name, $data)) {
+                    return $data[$name];
+                }
+
+                if ($parameter->isDefaultValueAvailable()) {
+                    return $parameter->getDefaultValue();
+                }
+
+                return null;
+            })
+            ->pipe(fn (DataCollection $data) => $reflection->newInstanceArgs($data->toArray()));
     }
 }
