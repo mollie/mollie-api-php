d04bc77d3b01d95ed79ea96c72bd388ad376240a
Naoray <10154100+Naoray@users.noreply.github.com>
2026-05-21 20:24:52 +0000

    Fixes coding style

--- src/Utils/Arr.php
+++ src/Utils/Arr.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Mollie\Api\Utils;
 
-use BackedEnum;
 use ArgumentCountError;
+use BackedEnum;
 use Mollie\Api\Contracts\QueryParameterBuilder;
 use Mollie\Api\Contracts\Stringable;
 
